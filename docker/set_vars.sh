2c75fe5067e5ea9a32282e7906934a3715523dc5
luxfabian <f.lux@fz-juelich.de>
2021-05-22 16:52:14 +0200

    docker setup

---
+++ docker/set_vars.sh
@@ -0,0 +1,7 @@
+
+#!/bin/bash
+
+source /opt/openfoam7/etc/bashrc
+
+export PYTHONPATH=$PYTHONPATH:/usr/local/lib/python3.8/site-packages/gmsh-4.8.4-Linux64-sdk/lib/
+export PATH=$PATH:/usr/local/lib/python3.8/site-packages/gmsh-4.8.4-Linux64-sdk/lib/
